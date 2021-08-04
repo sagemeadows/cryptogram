7b9e9fb3b3efb7597dca4df9f1c8a6e49bdc0e2f
sagemeadows <sage.b.meadows@gmail.com>
2021-08-04 15:44:04 -0700

    added shuffler

--- cryptomaker.py
+++ cryptomaker.py
@@ -5,12 +5,37 @@
 # usage:
 #   cryptomaker.py [words to scramble ...]
 #
-# 
+#
 
 import sys
+import random
+import numpy as np
 
 # print the arguments that were passed in via CLI
 arg_index = 0
 for arg in sys.argv:
     print("arg[{}] = '{}'".format(arg_index, arg))
     arg_index += 1
+
+foo = np.array([['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
+                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+                ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
+                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']])
+
+def shuffle(key_array):
+    state = ""
+    random.shuffle(key_array[1])
+    for i in range(len(key_array[0])):
+        if key_array[0][i] != key_array[1][i]:
+            state = "good"
+        else:
+            state = "bad"
+            break
+    if state == "bad":
+        random.shuffle(key_array[1])
+        shuffle(key_array)
+    else:
+        print("Cryptogram code is ready!")
+
+shuffle(foo)
+print(foo)
