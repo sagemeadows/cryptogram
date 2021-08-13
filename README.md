03fe6ebc03d4d2c89a2552450df5b28271480569
sagemeadows <sage.b.meadows@gmail.com>
2021-08-13 11:58:11 -0700

    removed numpy dependency

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 WKUUP, ZPGUJ! = Hello, World!
 
-Cryptograms are usually at least a sentence long, so there are enough repetitions of letters to solve the puzzle through trial and error. Some cryptograms offer a hint by telling you what one or two letters map to.
+Cryptograms are usually fairly long—at least one sentence, often more—so there are enough repetitions of letters to solve the puzzle through trial and error. Some cryptograms offer a hint by telling you what one or two letters map to.
 
 ## Create a cryptogram
 1. Download `cryptomaker.py`
@@ -14,21 +14,18 @@ Cryptograms are usually at least a sentence long, so there are enough repetition
 When you run `cryptomaker.py`, it will return a code, your original message, and the encoded message.
 
 ```
-user@computer:~/directory$ python cryptomaker.py Hello, World!
-
+user@computer:~/directory$ python cryptomaker.py The quick red fox jumps over the lazy brown dog.
 Your cryptogram code is:
 A  B  C  D  E  F  G  H  I  J  K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
-S  F  A  J  K  V  Q  W  M  C  O  U  D  I  P  B  N  G  L  R  X  E  Z  H  T  Y
+N  H  X  Q  I  Z  O  G  P  L  U  V  D  T  Y  W  K  S  C  J  A  M  F  E  R  B
 
 Your original message was:
-Hello, World!
+The quick red fox jumps over the lazy brown dog.
 
 Your new message is:
-WKUUP, ZPGUJ!
+JGI KAPXU SIQ ZYE LADWC YMIS JGI VNBR HSYFT QYO.
 
 Finished!
 ```
 
-**In order to use `cryptomaker.py`, you must have the following installed:**
-- python
-- numpy
+**In order to use `cryptomaker.py`, you must have python installed.**
--- cryptomaker.py
+++ cryptomaker.py
@@ -10,31 +10,30 @@
 import sys
 import string
 import random
-import numpy as np
 
 #cryptogram pre-key
-foo = np.array([['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-                ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
-                 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']])
+code = {"key": ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
+        "value": ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', \
+                  'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']}
 
 #create cryptogram key
-def shuffle(key_array):
+def shuffle(key_dict):
     state = ""
-    random.shuffle(key_array[1])
-    for i in range(len(key_array[0])):
-        if key_array[0][i] != key_array[1][i]:
+    random.shuffle(key_dict["value"])
+    for i in range(len(key_dict["key"])):
+        if key_dict["key"][i] != key_dict["value"][i]:
             state = "good"
         else:
             state = "bad"
             break
     if state == "bad":
-        random.shuffle(key_array[1])
-        shuffle(key_array)
+        random.shuffle(key_dict["value"])
+        shuffle(key_dict)
     else:
-        print("\nYour cryptogram code is:")
-        key = "  ".join(foo[0])
-        value = "  ".join(foo[1])
+        print("Your cryptogram code is:")
+        key = "  ".join(key_dict["key"])
+        value = "  ".join(key_dict["value"])
         print(key)
         print(value)
 
@@ -48,16 +47,16 @@ def arrange(message):
 def swap(message):
     print("\nYour original message was:\n" + message)
     key = "abcdefghijklmnopqrstuvwxyz"
-    value = "".join(foo[1])
+    value = "".join(code["value"])
     message = message.lower()
     for c in message:
         for i in range(len(key)):
-            if c.upper() == foo[0][i]:
+            if c.upper() == code["key"][i]:
                 message = message.replace(c, value[i])
                 break
     print("\nYour new message is:\n" + message)
     print("\nFinished!")
 
 #run program
-shuffle(foo)
+shuffle(code)
 arrange(sys.argv)
